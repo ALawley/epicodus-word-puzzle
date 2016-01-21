5ac6a95abc61d7019d780db3df9bb5abdd9d0318
Anna Lawley and Joshua Gustafson <silversevenfox@yahoo.com>
2016-01-21 09:54:46 -0800

    add function for wordPuzzle passed spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,26 @@
 // Example document ready function and form submit function w/ event handler to prevent default
-var countUp = function(firstNumber, secondNumber) {
-  var outputArray = [];
-  for (var index = firstNumber; index <= secondNumber; index += firstNumber) {
-    outputArray.push(index);
-  }
-  return outputArray;
-};
-
-
-
-
-$(document).ready(function() {
-  $("form").submit(function(event){
-    var firstNumber = parseInt($("input#firstNumber").val());
-    var secondNumber = parseInt($("input#secondNumber").val());
-    var outputs = countUp(firstNumber, secondNumber);
-    $("#output").empty()
-    outputs.forEach(function(output) {
-      $("#output").append("<li>" + output + "</li>")
-    });
-    event.preventDefault();
+var wordPuzzle = function(str) {
+  var vowelArray = ["a", "e", "i", "o", "u"];
+  vowelArray.forEach(function(vowel) {
+    var regex = new RegExp(vowel, 'gi');
+    str = str.replace(regex, "-");
   });
+  return str;
+}
 
-});
+
+
+
+// $(document).ready(function() {
+//   $("form").submit(function(event){
+//     var firstNumber = parseInt($("input#firstNumber").val());
+//     var secondNumber = parseInt($("input#secondNumber").val());
+//     var outputs = countUp(firstNumber, secondNumber);
+//     $("#output").empty()
+//     outputs.forEach(function(output) {
+//       $("#output").append("<li>" + output + "</li>")
+//     });
+//     event.preventDefault();
+//   });
+//
+// });
